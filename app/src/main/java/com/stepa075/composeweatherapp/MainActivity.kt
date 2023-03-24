a2f1f8a7d876469e283457380dc4412961e4e95b
Stepa075 <stepa0751@gmail.com>
2023-03-24 21:20:26 +0200

    lesson 12 complete

--- app/src/main/java/com/stepa075/composeweatherapp/MainActivity.kt
+++ app/src/main/java/com/stepa075/composeweatherapp/MainActivity.kt
@@ -3,7 +3,16 @@ package com.stepa075.composeweatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.stepa075.composeweatherapp.screens.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.stepa075.composeweatherapp.screens.MainCard
+
+import com.stepa075.composeweatherapp.screens.TabLayout
 import com.stepa075.composeweatherapp.ui.theme.ComposeWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +20,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeWeatherAppTheme {
-                MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.weather),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.FillBounds,
+                )
+              Column {
+                  MainCard()
+                  TabLayout()
+              }
+
+
             }
         }
     }
--- app/src/main/java/com/stepa075/composeweatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/stepa075/composeweatherapp/screens/MainScreen.kt
@@ -3,14 +3,13 @@ package com.stepa075.composeweatherapp.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -19,23 +18,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.stepa075.composeweatherapp.R
 import com.stepa075.composeweatherapp.ui.theme.BlueLight
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(
-        painter = painterResource(id = R.drawable.weather),
-        contentDescription = "im1",
-        modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.5f),
-        contentScale = ContentScale.FillBounds,
-    )
+fun MainCard() {
+
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding(5.dp)
     ) {
         Card(
@@ -82,13 +78,17 @@ fun MainScreen() {
                     style = TextStyle(fontSize = 15.sp),
                     color = Color.White
                 )
-                Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_search), contentDescription = "im3",
-                            tint = Color.White)
-                        
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+
                     }
                     Text(
                         text = "23°C/12°C",
@@ -97,13 +97,60 @@ fun MainScreen() {
                     )
 
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_sync), contentDescription = "im4",
-                        tint = Color.White)
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
 
                     }
-                    
+
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tablist = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(start = 5.dp, end = 5.dp)
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = {pos ->
+                        TabRowDefaults.Indicator(
+                            Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                        )
+            },
+            backgroundColor = BlueLight,
+            contentColor = Color.White
+        ) {
+            tablist.forEachIndexed { index, text ->
+                Tab(selected = false,
+                    onClick = {
+                             coroutineScope.launch {
+                                 pagerState.animateScrollToPage(index)
+                             }
+                    },
+                    text = {Text(text = text)}
+                )
+            }
+        }
+        HorizontalPager(count = tablist.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+            ) {
+                index ->
+
         }
     }
 }
